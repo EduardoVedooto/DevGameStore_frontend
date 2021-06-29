7919276f66edf0fd3150c81c242f22a987f73fe9
Eduardo Vedooto <vedootoeduardo@gmail.com>
2021-06-29 16:52:56 -0300

    Created signup page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Homescreen from "./pages/homescreen/index";
+import Signup from "./pages/sign-up";
+import Signin from "./pages/sign-in";
 import GlobalStyles from "./styles/GlobalStyles";
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
       <GlobalStyles />
       <Switch>
         <Route path="/" exact component={Homescreen} />
+        <Route path="/sign-up" exact component={Signup} />
+        <Route path="/sign-in" exact component={Signin} />
       </Switch>
     </Router>
   );
