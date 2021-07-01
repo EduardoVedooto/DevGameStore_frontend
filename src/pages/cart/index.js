5d8b01078d78dee109fb343d3a2d21c1746d92ff
Eduardo Vedooto <vedootoeduardo@gmail.com>
2021-06-30 22:13:01 -0300

    Layout Beta

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -1,13 +1,34 @@
-import { CartContainer } from "./style";
-import { BsPersonBoundingBox } from "react-icons/bs";
+import { CartContainer, ListItens, ListTitle, Item, OrderInfo } from "./style";
 import Header from "../../components/Header";
+import { useState } from "react";
 
 export default function Cart() {
-  const user = JSON.parse(sessionStorage.getItem("session")).user;
-  console.log(user);
+
+  const [gamesList, SetGamesList] = useState([]);
+
+
+  const user = JSON.parse(sessionStorage.getItem("session"))?.user;
+
   return (
     <CartContainer>
       <Header />
+      <ListItens>
+        <ListTitle>Seu carrinho</ListTitle>
+        {
+          gamesList.length ?
+            gamesList.map(game => (
+              <Item>
+
+              </Item>
+            ))
+            :
+            <h3>Seu carrinho está vazio</h3>
+        }
+        <Item>
+          <img src="#"></img>
+        </Item>
+      </ListItens>
+      <OrderInfo></OrderInfo>
     </CartContainer >
   );
 }
--- src/pages/cart/style.js
+++ src/pages/cart/style.js
@@ -2,9 +2,35 @@ import styled from "styled-components";
 
 export const CartContainer = styled.main`
   background-color: var(--color-dark);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  min-height: 100vh;
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  gap: 1rem;
+  min-height: calc(100vh - 10rem);
+  margin-top: 10rem;
+  padding: 0 5rem;
+`;
 
+export const ListTitle = styled.h2`
+  font-size: 2.4rem;
+  font-weight: 700;
+  color: var(--color-lighter);
+`;
+
+export const ListItens = styled.ul`
+  background-color: purple;
+  width: 100%;
+  height: 10rem;
+`;
+
+export const Item = styled.li`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  background-color: rosybrown;
+`;
+
+export const OrderInfo = styled.aside`
+  background-color: red;
+  width: 100%;
+  height: 10rem;
 `;
